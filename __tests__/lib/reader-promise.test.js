4e9fd8e72c2c077ab8ec8835520e5a44d37b7f75
Kyle Monahan <kyle.monahan2000@gmail.com>
2019-05-23 16:56:11 -0500

    set up files for promise changing from callbacks

---
+++ __tests__/lib/reader-promise.test.js
@@ -0,0 +1,47 @@
+'use strict';
+
+jest.mock('fs');
+
+const reader = require('../../lib/reader-promise');
+
+describe('File Reader Module', () => {
+
+  it('when given other than three files, returns an error', done => {
+    let files = ['bad.txt'];
+    // In jest, throwing errors obviously kills the app, so if you're
+    // going to throw one in a test, have the expect execute your code as a
+    // function so that you can trap it.
+    reader(files, (err) => {
+      expect(err).toBeDefined();
+      done();
+    });
+  });
+
+  it('when given a bad file, returns an error', done => {
+    let files = ['bad.txt', 'good.txt', 'good.txt'];
+    // In jest, throwing errors obviously kills the app, so if you're
+    // going to throw one in a test, have the expect execute your code as a
+    // function so that you can trap it.
+    reader(files, (err) => {
+      expect(err).toBeDefined();
+      done();
+    });
+  });
+
+
+  it('reads 3 files', done => {
+    let files = ['file1.txt', 'file2.txt', 'file3.txt'];
+    reader(files, (err,data) => {
+      expect(err).toBeNull();
+      expect(data instanceof Array ).toBeTruthy();
+      expect(data.length ).toBe(3);
+      expect(data).toEqual([
+        'file1.txt contents',
+        'file2.txt contents',
+        'file3.txt contents',
+      ]);
+      done();
+    });
+  });
+
+});
